ae2f24f8ba0b2e552bcaac0c630ade3be96c6b2d
Coen Stange <coen.stange@hotmail.com>
2017-11-19 20:42:13 +0100

    + add transitions to graph

--- ale/app/sagas/statemachinegraph.js
+++ ale/app/sagas/statemachinegraph.js
@@ -2,11 +2,26 @@ import { takeEvery, put } from 'redux-saga/effects';
 import { stateMachineGraph } from '../actions/statemachine';
 
 function* createGraph({ statemachine }) {
+  // begin with static first line
   let dotfile = ['digraph {', 'rankdir=LR;'];
+  // add every state
   dotfile = dotfile.concat(
     Object.keys(statemachine.states)
       .map(x => `"${x}" [shape=${statemachine.states[x].final ? 'doublecircle' : 'circle'}]`)
   );
+  // Add transitions
+  Object.keys(statemachine.states).some(s => { // foreach state
+    const state = statemachine.states[s];
+    if (state.initial) {
+      dotfile = [...dotfile, `"" -> "${s}"`];
+    }
+    dotfile = dotfile.concat(
+      state.transitions
+        .map(x => `"${s}" -> "${x.to}" [label="${x.character}"]`)
+    );
+    return false; // keep looping
+  });
+  // Add last line
   dotfile = [...dotfile, '}'];
   yield put(stateMachineGraph(dotfile.join('')));
 }
